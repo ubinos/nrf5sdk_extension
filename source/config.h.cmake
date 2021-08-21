063c5b75ea86e40e69052cdea6723c54ee104f73
Sungho Park <siminsungho@gmail.com>
2021-08-21 11:59:22 +0900

    initial commit

---
+++ source/config.h.cmake
@@ -0,0 +1,9 @@
+////////
+
+
+#cmakedefine01 INCLUDE__NRF5SDK_EXTENSION
+
+#if (INCLUDE__NRF5SDK_EXTENSION == 1)
+
+#endif /* (INCLUDE__NRF5SDK_EXTENSION == 1) */
+
---
+++ source/sources.cmake
@@ -0,0 +1,12 @@
+if(INCLUDE__NRF5SDK_EXTENSION)
+
+    get_filename_component(_tmp_source_dir "${CMAKE_CURRENT_LIST_DIR}" ABSOLUTE)
+    file(GLOB_RECURSE _tmp_sources
+        "${_tmp_source_dir}/*.c"
+        "${_tmp_source_dir}/*.cpp"
+        "${_tmp_source_dir}/*.S"
+        "${_tmp_source_dir}/*.s")
+    set(PROJECT_SOURCES ${PROJECT_SOURCES} ${_tmp_sources})
+
+endif(INCLUDE__NRF5SDK_EXTENSION)
+
